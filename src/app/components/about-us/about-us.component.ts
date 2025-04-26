615f8cb97d373f83773fb294c4ddebb8a68c139c
kjeancharlesPro <kjeancharles@hn-services.fr>
2025-04-26 12:10:33 +0200

    update color theme

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,19 +1,24 @@
-
 import { Component } from '@angular/core';
-import { MissionsComponent } from "./missions/missions.component";
-import { HistoryComponent } from "./history/history.component";
-import { SoinsComponent } from "./soins/soins.component";
-import { TestimonyComponent } from "./testimony/testimony.component";
-import { FinanceComponent } from "./finance/finance.component";
-import { PartenariaComponent } from "./partenaria/partenaria.component";
-import { BrefComponent } from "./bref/bref.component";
+import { MissionsComponent } from './missions/missions.component';
+import { HistoryComponent } from './history/history.component';
+import { SoinsComponent } from './soins/soins.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { FinanceComponent } from './finance/finance.component';
+import { PartenariaComponent } from './partenaria/partenaria.component';
+import { BrefComponent } from './bref/bref.component';
 
 @Component({
   selector: 'app-about-us',
-  imports: [MissionsComponent, HistoryComponent, SoinsComponent, TestimonyComponent, FinanceComponent, PartenariaComponent, BrefComponent],
+  imports: [
+    MissionsComponent,
+    HistoryComponent,
+    SoinsComponent,
+    TestimonyComponent,
+    FinanceComponent,
+    PartenariaComponent,
+    BrefComponent,
+  ],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+  styleUrl: './about-us.component.scss',
 })
-export class AboutUsComponent {
-
-}
+export class AboutUsComponent {}
