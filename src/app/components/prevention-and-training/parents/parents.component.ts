73b23db62169235885f78c35fbb5feac9298b4b0
kjeancharlesPro <kjeancharles@hn-services.fr>
2025-04-30 11:20:59 +0200

    add parents page

--- src/app/components/prevention-and-training/parents/parents.component.ts
+++ src/app/components/prevention-and-training/parents/parents.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-parents',
   imports: [],
   templateUrl: './parents.component.html',
-  styleUrl: './parents.component.scss'
+  styleUrl: './parents.component.scss',
 })
 export class ParentsComponent {
-
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
