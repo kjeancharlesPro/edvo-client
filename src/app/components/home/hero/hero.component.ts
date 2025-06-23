8b610f333b1dfd7fbdc944c287143cb2218bf73d
kjeancharlesPro <kjeancharles@hn-services.fr>
2025-06-23 21:14:33 +0200

    final commit

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  counter = 0;
+  intervalId: NodeJS.Timeout | undefined;
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.counter++;
+    }, 1000);
+  }
 }
